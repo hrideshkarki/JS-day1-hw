8af66eeb43e2e43007c8a5c2fd43589864bbdbf4
Hridesh Karki <hridesh@Hrideshs-MBP-2.lan>
2023-04-25 22:30:57 -0400

    codewars js solutions

---
+++ JS-day1-hw2.js
@@ -0,0 +1,17 @@
+// This code does not execute properly. Try to figure out why.
+
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
+
+
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+public class EvenOrOdd {
+    public static String even_or_odd(int number) {
+        if(number%2==0) return "Even"; else return "Odd";        
+    }
+}
